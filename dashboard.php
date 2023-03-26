477d91ad815af74fb0fb9d5bf382a3e7b12ce70c
SachinPrab <124426750+SachinPrab@users.noreply.github.com>
2023-03-26 12:19:08 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,15 @@
 <?php 
 $Username = $_POST["username"];
-  $Password = $_POST["password"];
- if ($Username == 'admin' && $Password == 'password123') {
+$Password = $_POST["password"];
+
+// Define keys before accessing them
+if (!isset($_POST["username"]) || !isset($_POST["password"])) {
+    // Handle missing fields
+    echo "Please enter a username and password.";
+} else if ($Username == 'admin' && $Password == 'password123') {
     header('Location: dashboard.php');
     exit;
-  } else {
+} else {
     echo "Invalid username or password.";
-  }
+}
 ?>
