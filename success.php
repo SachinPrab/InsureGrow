d640e5467ad21026bf9eff8672f06f9b229bd6d4
SachinPrab <124426750+SachinPrab@users.noreply.github.com>
2023-03-26 09:34:46 +0530

    Create success.php

---
+++ success.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Success!</title>
+</head>
+<body>
+	<h1>Thank you for signing up!</h1>
+	<p>Your account has been created successfully.</p>
+</body>
+</html>
