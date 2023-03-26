870f1d9f6d54269cd66ab63a81cc17c11a0bb248
SachinPrab <124426750+SachinPrab@users.noreply.github.com>
2023-03-26 10:03:03 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -3,8 +3,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	// Get the form data
 	$name = $_POST["name"];
 	$username = $_POST["username"];
-	$email = $_POST["email"];
 	$password = $_POST["password"];
+	$email = $_POST["email"];
 	$address = $_POST["address"];
 	$phone_number = $_POST["phone_number"];
 	$date_of_birth = $_POST["date_of_birth"];
