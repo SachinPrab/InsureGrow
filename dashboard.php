36e64d508266a035e48e3d79a93ac0aee5524f47
nagarjuna923000 <78395108+nagarjuna923000@users.noreply.github.com>
2023-04-04 09:36:58 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -7,7 +7,7 @@ if (!isset($_POST["username"]) || !isset($_POST["password"])) {
     // Handle missing fields
     echo "Please enter a username and password.";
 } else if ($Username == 'admin' && $Password == 'password123') {
-    header('Location: microinv1.html');
+    header('Location: file:///C:/Users/Nagu/Documents/microinvestment%20githhub/microinvestment-14/microinv1.html');
     exit;
 } else {
     echo "Invalid username or password.";
