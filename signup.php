76c62a627106c5eeeb69010e07d0da17053d24df
SachinPrab <124426750+SachinPrab@users.noreply.github.com>
2023-04-04 20:51:48 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -10,8 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	$date_of_birth = $_POST["date_of_birth"];
 	$occupation = $_POST["occupation"];
 	$annual_income = $_POST["annual_income"];
-    header("Location: success.php");
-    exit();
+    
 }
 }
 ?>
