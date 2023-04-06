e0e6b78d350b4c392f2a11191611a6304e7ff496
SachinPrab <124426750+SachinPrab@users.noreply.github.com>
2023-04-06 11:38:21 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -14,4 +14,22 @@ else
 {
     echo "User details have not been added";
 }
+$sql = "SELECT username, password FROM login'";
+$result = mysqli_query($con, $sql);
+
+// Check if the query was successful
+if ($result === false) {
+    die("Query failed: " . mysqli_error($conn));
+}
+
+// Check if any rows were returned
+if (mysqli_num_rows($result) > 0) {
+    // Fetch the row as an associative array
+    $row = mysqli_fetch_assoc($result);
+    echo "Username: " . $row['username'] . "<br>";
+    echo "Password: " . $row['password'] . "<br>";
+} else {
+    echo "User not found";
+}
+mysqli_close($conn);
 ?>
