b196eec24e8a24e69147ca07fed05a707befd996
SachinPrab <124426750+SachinPrab@users.noreply.github.com>
2023-04-10 19:05:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,19 @@
 from flask import Flask, render_template
 import requests
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder= "C:\\Users\\User\\Desktop\\microinvestment\\microinvestment 3\\microinvestment-8\\templates")
 
-@app.route('/stock.html')
+@app.route('/')
 def home():
     # Define the API endpoint URL
-    url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=RELIANCE.BSE&outputsize=full&apikey=Q2DUWBMNUYAWW57M"
-
+    url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=RELIANCE.BSE&outputsize=full&apikey=Q2DUWBMNUYAWW57M'
+    r = requests.get(url)
+    data = r.json()
+    for date, values in data['Time Series (Daily)'].items():
+        values['1. open'] = float(values['1. open'])
+        values['2. high'] = float(values['2. high'])
+        values['3. low'] = float(values['3. low'])
+        values['4. close'] = float(values['4. close'])
     # Send a GET request to the API endpoint
     response = requests.get(url)
 
@@ -15,12 +21,12 @@ def home():
     if response.status_code == 200:
         # Extract the JSON response data
         data = response.json()
-
         # Pass the data to the HTML template
-        return render_template('stocks.html', data=data)
+        return render_template('stocks.html', data=data['Time Series (Daily)'])
     else:
         # If the request was not successful, return an error message
         return "Error: " + str(response.status_code)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
