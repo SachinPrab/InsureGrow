bc0183e6858a6902e0ffe91dd8ee244311a1022e
SachinPrab <124426750+SachinPrab@users.noreply.github.com>
2023-04-17 20:10:14 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -29,28 +29,11 @@ $sql="INSERT INTO login(username,password) values('$u','$p')";
 $r = mysqli_query($con,$sql);
 if($r)
 {
-    echo "User details stored successfully" . "<br>";
+    echo "Thank you for registering with our website, $name. Welcome to InsureGrow. Please click <a href='homepage.html'>here</a> to continue.";
 }
 else
 {
     echo "User details have not been added";
 }
-$sql = "SELECT username FROM login where username = '$u'";
-$result = mysqli_query($con, $sql);
-
-// Check if the query was successful
-if ($result === false) {
-    die("Query failed: " . mysqli_error($con));
-}
-
-// Check if any rows were returned
-if (mysqli_num_rows($result) > 0) {
-    // Fetch the row as an associative array
-    $row = mysqli_fetch_assoc($result);
-    echo "Welcome " . $row['username'] . "<br>";
-} else {
-    echo "User not found";
-}
-mysqli_close($con);
 ?>
         
