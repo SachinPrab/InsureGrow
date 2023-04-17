6f9d3d92aa1294523c207ae31cb8469b869bde04
SachinPrab <124426750+SachinPrab@users.noreply.github.com>
2023-04-17 20:08:47 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -16,36 +16,11 @@ $sql="INSERT INTO signup(name,username,password,email,address,phone_number,date_
 	$q = mysqli_query($con,$sql);
 	if($q)
 	{
-		echo "Thank you for registering with our website,Welcome to InsureGrow";
+		echo "Thank you for registering with our website, $name. Welcome to InsureGrow. Please click <a href='homepage.html'>here</a> to continue.";
 	}
 	else
 	{
 		echo "User details have not been added in database";
 	}
-}
-$sql = "SELECT name, username, password,email,address, phone_number, date_of_birth, occupation, annual_income FROM signup";
-$result = mysqli_query($con, $sql);
-
-if ($result === false) {
-    die("Query failed: " . mysqli_error($con));
-}
-// Check if there are any results
-if (mysqli_num_rows($result) > 0) {
-    // Output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "Name: " . $row["name"]. "<br>";
-        echo "Username: " . $row["username"]. "<br>";
-        echo "Password: " . $row["password"]. "<br>";
-	echo "Email: " . $row["email"]. "<br>";
-        echo "Address: " .$row["address"]. "<br>";
-        echo "Phone Number: " . $row["phone_number"]. "<br>";
-        echo "Date of Birth: " . $row["date_of_birth"]. "<br>";
-        echo "Occupation: " . $row["occupation"]. "<br>";
-        echo "Annual Income: " . $row["annual_income"]. "<br>";
-        echo "<br>";
-    }
-} 
-	else {
-    echo "No results found";
 }
 ?>
